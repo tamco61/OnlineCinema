eb9bdc19bc397a25b5de64a54e0bfdf0521efb76
tamco61 <42232243+tamco61@users.noreply.github.com>
2025-12-23 22:10:36 +0300

    rerun

--- backend/models/country.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class Country(BaseModel):
-    pass
--- backend/models/media.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class Media(BaseModel):
-    pass
-
-
-class MediaFile(BaseModel):
-    pass
