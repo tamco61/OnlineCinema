7d7f3eeb4d29e8bb6949c46b112c707bc162919e
tamco61 <42232243+tamco61@users.noreply.github.com>
2025-12-26 21:06:12 +0300

    init fastapi temp

---
+++ services/user/app/main.py
@@ -0,0 +1,35 @@
+# remote module
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+import logging
+
+# local module
+from core.config import settings
+
+
+logger = logging.getLogger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Lifespan context manager."""
+    logger.info(f"Starting {settings.SERVICE_NAME} v{settings.SERVICE_VERSION}")
+
+    yield
+
+    logger.info(f"Shutdown {settings.SERVICE_NAME} v{settings.SERVICE_VERSION}")
+
+app = FastAPI(
+    lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# app.include_router()
