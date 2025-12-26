8f81de9ecb55546370975dc40649576c62ccb2a0
tamco61 <42232243+tamco61@users.noreply.github.com>
2025-12-26 20:53:11 +0300

    add simple config + env

---
+++ services/streaming/app/core/config.py
@@ -0,0 +1,27 @@
+from functools import lru_cache
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore"
+    )
+
+    SERVICE_NAME: str = Field(default="streaming-service")
+    SERVICE_VERSION: str = Field(default="1.0.0")
+
+    HOST: str = Field(default="localhost")
+    PORT: int = Field(default=8005)
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
+
+
+settings = get_settings()
---
+++ services/user/app/core/config.py
@@ -0,0 +1,27 @@
+from functools import lru_cache
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore"
+    )
+
+    SERVICE_NAME: str = Field(default="user-service")
+    SERVICE_VERSION: str = Field(default="1.0.0")
+
+    HOST: str = Field(default="localhost")
+    PORT: int = Field(default=8002)
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
+
+
+settings = get_settings()
