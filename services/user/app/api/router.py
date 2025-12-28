985afd241022fd3e79549f34230fa40d77de0f7b
tamco61 <42232243+tamco61@users.noreply.github.com>
2025-12-28 18:41:05 +0300

    add routers and points

---
+++ services/user/app/api/router.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+from app.api.endpoints import favorites, history, user
+
+router = APIRouter()
+
+router.include_router(user.router, prefix="/user")
+router.include_router(history.router, prefix="/user")
+router.include_router(favorites.router, prefix="/user")
